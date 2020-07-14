5b9177b6a65c6d951ddd50cc517aae623659c17f
SeungYooon <tmddbs4998@naver.com>
2020-07-14 15:48:11 +0900

    set error action in glide

--- app/src/main/java/com/example/mytoyproject/main/view/adapter/BindingAdapter.kt
+++ app/src/main/java/com/example/mytoyproject/main/view/adapter/BindingAdapter.kt
@@ -3,8 +3,9 @@ package com.example.mytoyproject.main.view.adapter
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
+import com.example.mytoyproject.R
 
 @BindingAdapter("avatarUrl")
 fun bindImageFromRes(view: ImageView, string: String?) {
-    Glide.with(view.context).load(string).override(1024).into(view)
+    Glide.with(view.context).load(string).error(R.mipmap.ic_launcher).override(1024).into(view)
 }
