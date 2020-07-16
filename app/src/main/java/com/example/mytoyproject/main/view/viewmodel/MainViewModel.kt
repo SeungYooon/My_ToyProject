6996fe81fba3ebf8a0a5f273ee0a05daab406b60
SeungYooon <tmddbs4998@naver.com>
2020-07-16 16:34:27 +0900

    set ViewModel

--- app/src/main/java/com/example/mytoyproject/main/view/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/mytoyproject/main/view/viewmodel/MainViewModel.kt
@@ -1,34 +1,47 @@
 package com.example.mytoyproject.main.view.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
+import com.example.mytoyproject.base.BaseKotlinViewModel
 import com.example.mytoyproject.main.view.repository.MainRepository
+import com.example.mytoyproject.network.api.RepoService
+import com.example.mytoyproject.network.model.GithubItem
 import com.example.mytoyproject.network.model.Item
+import com.example.mytoyproject.network.model.RepoModel
 import com.example.mytoyproject.utils.Resource
+import com.example.mytoyproject.utils.SingleLiveEvent
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 import kotlinx.coroutines.Dispatchers
 
-class MainViewModel(private val mainRepository: MainRepository) : ViewModel() {
+class MainViewModel(private val model: RepoModel) : BaseKotlinViewModel() {
 
-    fun getRepo(name: String) = liveData(Dispatchers.IO) {
-        emit(Resource.loading(data = null))
-        try {
-            emit(Resource.success(data = mainRepository.getRepo(name)))
-        } catch (exception: Exception) {
-            emit(Resource.error(data = null, msg = exception.message ?: "Error Occurred!"))
-        }
-    }
+    private val TAG = "MainViewModel"
 
-    fun getOwner(owner: String, repo: String) = liveData(Dispatchers.IO) {
-        emit(Resource.loading(data = null))
-        try {
-            emit(Resource.success(data = mainRepository.getOwner(owner, repo)))
-        } catch (exception: Exception) {
-            emit(Resource.error(data = null, msg = exception.message ?: "Error Occurred!"))
-        }
+    private val _repoServiceLiveData = MutableLiveData<GithubItem>()
+    val repoServiceLiveData: LiveData<GithubItem>
+        get() = _repoServiceLiveData
+
+    fun getRepo(name: String) {
+        addDisposable(
+            model.getRepo(name)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    it.run {
+                        if (totalCount > 0) {
+                            Log.d(TAG, "totalcount : $totalCount")
+                            _repoServiceLiveData.postValue(this)
+                        }
+                        Log.d(TAG, "items : $items")
+                    }
+                }, {
+                    Log.d(TAG, "response error, message : ${it.message}")
+                })
+        )
     }
 }
--- app/src/main/java/com/example/mytoyproject/main/view/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/example/mytoyproject/main/view/viewmodel/UserViewModel.kt
@@ -1,26 +1,84 @@
 package com.example.mytoyproject.main.view.viewmodel
 
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.liveData
+import com.example.mytoyproject.R
+import com.example.mytoyproject.base.BaseKotlinViewModel
 import com.example.mytoyproject.main.view.repository.UserRepository
+import com.example.mytoyproject.network.api.UserService
+import com.example.mytoyproject.network.model.GithubItem
+import com.example.mytoyproject.network.model.Item
 import com.example.mytoyproject.network.model.User
+import com.example.mytoyproject.network.model.UserModel
 import com.example.mytoyproject.utils.Resource
+import com.example.mytoyproject.utils.SingleLiveEvent
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 import kotlinx.coroutines.Dispatchers
 import java.lang.Exception
 
-class UserViewModel(private val userRepository: UserRepository) : ViewModel() {
+class UserViewModel(private val model: UserModel) : BaseKotlinViewModel() {
 
-    fun getUser(username: String) = liveData(Dispatchers.IO) {
-        emit(Resource.loading(data = null))
-        try {
-            emit(Resource.success(data = userRepository.getUser(username)))
-        } catch (exception: Exception) {
-            emit(Resource.error(data = null, msg = exception.message ?: "Error Occurred!"))
-        }
+    companion object {
+        private const val TAG = "UserViewModel"
+    }
+
+    private val _ownerServiceLiveData = MutableLiveData<Item>()
+    val ownerServiceLiveData: LiveData<Item>
+        get() = _ownerServiceLiveData
+
+    private val _userServiceLiveData = MutableLiveData<User>()
+    val userServiceLiveData: LiveData<User>
+        get() = _userServiceLiveData
+
+    private val _startEvent = SingleLiveEvent<Any>()
+    val startEvent: LiveData<Any>
+        get() = _startEvent
+
+    fun getOwner(owner: String, repo: String) {
+        addDisposable(
+            model.getOwner(owner, repo)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    it.run {
+                        if (owner != null) {
+                            Log.d(TAG, "Owner Status : ${it.description}")
+                            _ownerServiceLiveData.postValue(this)
+                        }
+                    }
+                }, {
+                    Log.d(TAG, "response error, message : ${it.message}")
+                })
+        )
+    }
+
+    fun getUser(username: String) {
+        addDisposable(
+            model.getUser(username)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    it.run {
+                        if (followers >= 0 && following >= 0) {
+                            Log.d(TAG, "User Status : $followers +','+ $following")
+                            _userServiceLiveData.postValue(this)
+                        }
+                    }
+                }, {
+                    Log.d(TAG, "response error, message : ${it.message}")
+                })
+        )
+    }
+
+    fun doSomething() {
+        _startEvent.call()
     }
 }
+
