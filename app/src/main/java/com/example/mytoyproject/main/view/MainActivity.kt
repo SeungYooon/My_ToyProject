491598c17676a2c37a1b9fcb727c02636fd20ee3
SeungYooon <tmddbs4998@naver.com>
2020-07-16 16:39:15 +0900

    set MainActivity using MVVM pattern

--- app/src/main/java/com/example/mytoyproject/main/view/MainActivity.kt
+++ app/src/main/java/com/example/mytoyproject/main/view/MainActivity.kt
@@ -1,36 +1,32 @@
 package com.example.mytoyproject.main.view
 
 import android.content.BroadcastReceiver
+import org.koin.androidx.viewmodel.ext.android.viewModel
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import android.net.ConnectivityManager
-import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
-import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.example.mytoyproject.R
-import com.example.mytoyproject.base.ViewModelFactory
+import com.example.mytoyproject.base.BaseKotlinActivity
 import com.example.mytoyproject.main.view.adapter.GitAdapter
 import com.example.mytoyproject.databinding.ActivityMainBinding
 import com.example.mytoyproject.main.view.viewmodel.MainViewModel
-import com.example.mytoyproject.network.api.RepoHelper
-import com.example.mytoyproject.network.api.RetrofitHelper
-import com.example.mytoyproject.network.model.Item
-import com.example.mytoyproject.utils.Status
 import kotlinx.android.synthetic.main.activity_main.*
+import org.koin.android.ext.android.inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseKotlinActivity<ActivityMainBinding, MainViewModel>() {
 
-    private lateinit var binding: ActivityMainBinding
-    private lateinit var gitAdapter: GitAdapter
-    private lateinit var mainViewModel: MainViewModel
+    override val layoutResourceId: Int
+        get() = R.layout.activity_main
 
+    override val viewModel: MainViewModel by viewModel()
+    private val gitAdapter: GitAdapter by inject()
+
+    // if not connected Internet
     private var broadcastReceiver: BroadcastReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
             val notConnected =
@@ -43,63 +39,42 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-
-        setupViewModel()
-
-        gitAdapter = GitAdapter(arrayListOf())
-        binding.recyclerView.adapter = gitAdapter
-        binding.recyclerView.setHasFixedSize(true)
-
-        binding.imgSearch.setOnClickListener {
-            loadData()
-            hideKeyboard()
+    override fun initStartView() {
+        recyclerView.run {
+            adapter = gitAdapter
+            setHasFixedSize(true)
         }
     }
 
-    private fun setupViewModel() {
-        mainViewModel = ViewModelProviders.of(
-            this, ViewModelFactory(RepoHelper(RetrofitHelper.repoService))
-        ).get(MainViewModel::class.java)
+    override fun initDataBinding() {
+        viewModel.repoServiceLiveData.observe(this, Observer {
+            it.items.forEach { item ->
+                gitAdapter.updateRepo(it.items)
+            }
+            gitAdapter.notifyDataSetChanged()
+        })
     }
 
-    private fun loadData() {
-        if (binding.editSearch.text.isEmpty()) {
-            Toast.makeText(this, R.string.emptySearch, Toast.LENGTH_LONG).show()
-        } else {
-            mainViewModel.getRepo(editSearch.text.toString()).observe(this, Observer {
-                it.let { resources ->
-                    when (resources.status) {
-                        Status.SUCCESS -> {
-                            pbGone()
-                            loadSuccess()
-                            resources.data?.let { item -> renderList(item.items) }
-                        }
-                        Status.LOADING -> {
-                            pbShow()
-                        }
-                        Status.ERROR -> {
-                            pbGone()
-
-                            it.data?.let { GithubItem ->
-                                if (GithubItem.totalCount == 0) {
-                                    loadFail(t = Throwable())
-                                }
-                            }
-                        }
-                    }
+    override fun initAfterBinding() {
+        imgSearch.setOnClickListener {
+            pbShow()
+            if (editSearch.text.isEmpty()) {
+                pbGone()
+                Toast.makeText(this, R.string.emptySearch, Toast.LENGTH_LONG).show()
+            } else {
+                hideKeyboard()
+                if (editSearch.text.toString() != null) {
+                    loadRepo()
+                } else {
+                    loadFail()
                 }
-            })
+            }
         }
     }
 
-    private fun renderList(repos: ArrayList<Item>) {
-        gitAdapter.apply {
-            updateRepo(repos)
-        }
+    private fun loadRepo() {
+        loadSuccess()
+        viewModel.getRepo(editSearch.text.toString())
     }
 
     private fun hideKeyboard() {
@@ -113,33 +88,33 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun loadSuccess() {
-        binding.txtNoResponse.visibility = View.INVISIBLE
-        binding.recyclerView.visibility = View.VISIBLE
+        txtNoResponse.visibility = View.INVISIBLE
+        recyclerView.visibility = View.VISIBLE
     }
 
-    private fun loadFail(t: Throwable) {
-        binding.recyclerView.visibility = View.INVISIBLE
-        binding.txtNoResponse.visibility = View.VISIBLE
+    private fun loadFail() {
+        recyclerView.visibility = View.INVISIBLE
+        txtNoResponse.visibility = View.VISIBLE
         Toast.makeText(this@MainActivity, "No result for request!", Toast.LENGTH_SHORT).show()
     }
 
     private fun pbShow() {
-        binding.progressBar.visibility = View.VISIBLE
-        binding.recyclerView.visibility = View.GONE
+        progressBar.visibility = View.VISIBLE
+        recyclerView.visibility = View.GONE
     }
 
     private fun pbGone() {
-        binding.progressBar.visibility = View.GONE
+        progressBar.visibility = View.GONE
     }
 
     private fun connected() {
-        binding.recyclerView.visibility = View.VISIBLE
-        binding.txtNoConnection.visibility = View.INVISIBLE
+        recyclerView.visibility = View.VISIBLE
+        txtNoConnection.visibility = View.INVISIBLE
     }
 
     private fun disconnected() {
-        binding.recyclerView.visibility = View.INVISIBLE
-        binding.txtNoConnection.visibility = View.VISIBLE
+        recyclerView.visibility = View.INVISIBLE
+        txtNoConnection.visibility = View.VISIBLE
     }
 
     override fun onStart() {
