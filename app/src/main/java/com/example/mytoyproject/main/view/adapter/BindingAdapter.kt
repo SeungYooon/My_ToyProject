6b07567c95529741aa3d70240dd5ff6750159362
SeungYooon <tmddbs4998@naver.com>
2020-07-16 16:38:24 +0900

    using GlideModule

--- app/src/main/java/com/example/mytoyproject/main/view/adapter/BindingAdapter.kt
+++ app/src/main/java/com/example/mytoyproject/main/view/adapter/BindingAdapter.kt
@@ -4,8 +4,11 @@ import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.example.mytoyproject.R
+import com.example.mytoyproject.utils.GlideApp
+import com.example.mytoyproject.utils.MyGlide
 
 @BindingAdapter("avatarUrl")
 fun bindImageFromRes(view: ImageView, string: String?) {
-    Glide.with(view.context).load(string).error(R.mipmap.ic_launcher).override(1024).into(view)
+    GlideApp.with(view.context).load(string).placeholder(R.drawable.ic_search)
+        .error(R.mipmap.ic_launcher).override(1024).into(view)
 }
